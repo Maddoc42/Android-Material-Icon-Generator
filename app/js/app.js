1035bbe7970b8e7d7fa760f700bd71e0949785b5
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2016-01-18 14:54:21 +0100

    Move draw methods to drawUtils

--- app/js/app.js
+++ app/js/app.js
@@ -9,48 +9,6 @@ const
 
 var App = {
 
-    drawBounds: function(item, color) {
-        var bounds = item.strokeBounds;
-        var rect = new paper.Shape.Rectangle(
-            bounds.point,
-            bounds.size
-        );
-        rect.strokeColor = color;
-    },
-
-
-    drawPoint: function(point, color) {
-        var circle = new paper.Shape.Circle(point, 5);
-        circle.strokeColor = color;
-        circle.fillColor = color;
-    },
-
-
-    drawHandle: function(point, handle, color) {
-        var offset = point.add(handle);
-        var line = new paper.Path.Line(point, offset);
-        line.strokeColor = color;
-        line.strokeWidth = 2;
-    },
-
-
-    drawSegment: function(segment, color) {
-        drawPoint(segment.point, color);
-        if (segment.handleIn) drawPoint(segment.handleIn, color);
-        if (segment.handleOut) drawPoint(segment.handleOut, color);
-    },
-
-
-    drawCurve: function(curve, color) {
-        drawPoint(curve.point1, color);
-        // drawPoint(curve.point2, color);
-        // if (curve.handle1) drawPoint(curve.handle1, color);
-        // if (curve.handle2) drawPoint(curve.handle1, color);
-        if (curve.handle1) drawHandle(curve.point1, curve.handle1, color);
-        if (curve.handle2) drawHandle(curve.point2, curve.handle2, color);
-    },
-
-
     simplifyPath: function(path) {
         var offsetDistance = Math.round(100 * Math.sqrt(SHADOW_OFFSET * SHADOW_OFFSET * 2)) / 100;
         var doubleOffsetDistance = Math.round(100 * Math.sqrt(SHADOW_OFFSET * SHADOW_OFFSET * 2) * 2) / 100;

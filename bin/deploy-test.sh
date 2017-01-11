2c5e00f00971b955906cbfc02bf4f6cced3ce81f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2017-01-11 18:40:24 +0100

    Add test deployment script

---
+++ bin/deploy-test.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+repoDir=/tmp/material-android-icons-deploy
+buildDir=./public
+version=`grep -Po '"'"version"'"\s*:\s*"\K([^"]*)' package.json`
+
+# pre-cleanup
+rm -rf $repoDir
+
+# clone repo
+git clone -b deployment-development git@github.com:Maddoc42/MaterialIconsGenerator.git $repoDir
+echo
+
+# build project
+brunch build --production
+echo
+
+# cp files
+cd $repoDir
+git rm -r *
+cd -
+cp -r $buildDir/* $repoDir
+echo
+
+# commit + push
+cd $repoDir
+git add --all .
+git commit -m "Publish version $version"
+git push
+cd -
+echo
+
+# cleanup
+rm -rf $repoDir
+echo
